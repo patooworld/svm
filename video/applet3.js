037c82a376e0f7b1c9cf051afec7292b3e9cecb9
SAMUEL CHIBUNNA <info@patooworld.com>
2024-12-21 17:08:08 -0800

    Create applet3.js

---
+++ video/applet3.js
@@ -0,0 +1,9 @@
+function setup() {
+    let canvas = createCanvas(200, 200);
+     canvas.parent("applet3-container");
+}
+function draw() {
+    background(200, 20, 100);
+    line(0,0,200,200);
+  
+}

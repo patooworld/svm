f640b7c74074c1bfa7956b3c031693b80a45ed17
SAMUEL CHIBUNNA <info@patooworld.com>
2024-12-21 17:06:54 -0800

    Create applet2.js

---
+++ spatial/applet2.js
@@ -0,0 +1,10 @@
+function setup() {
+    let canvas = createCanvas(200, 200);
+     canvas.parent("applet2-container");
+}
+function draw() {
+  background(0);
+  rectMode(CENTER);
+  fill(255);
+  rect(100,100, 50, 50);
+}
